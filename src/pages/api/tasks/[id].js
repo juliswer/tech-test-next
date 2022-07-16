9a5f3e9b1d89452ff4e7a76fb9da3f3f06730322
juliswer <julianswer36@gmail.com>
2022-07-16 13:40:00 -0300

    uploading toggle done

--- src/pages/api/tasks/[id].js
+++ src/pages/api/tasks/[id].js
@@ -20,15 +20,23 @@ export default async function hanlderId(req, res) {
         return res.status(500).json({ error });
       }
     case "PUT":
+
+      const bodyUpdated = {
+        title: body.title,
+        description: body.description,
+        done: body.done,
+      };
+
       try {
-        const task = await Task.findByIdAndUpdate(id, body, {
-          new: true
+        const task = await Task.findByIdAndUpdate(id, bodyUpdated, {
+          new: true,
         });
         if (!task) return res.status(404).json({ error: "Task not found" });
         return res.status(200).json(task);
       } catch (error) {
         return res.status(500).json({ error });
       }
+      
     case "DELETE":
       try {
         const task = await Task.findByIdAndDelete(id);
