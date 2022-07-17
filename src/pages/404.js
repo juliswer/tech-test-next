72f7a951f043c7a13dbd84659916b9e1683ac16b
juliswer <julianswer36@gmail.com>
2022-07-16 22:08:59 -0300

    upload model config

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ function NotFound() {
     <>
       <h2>h</h2>
       <div style={{height: "100%", width: "100%"}}>
-        <Canvas camera={{ zoom: 30, position: [15, 20, 15] }}>
+        <Canvas camera={{ fov: 35, zoom: 20, position: [25, 20, 15] }}>
           <ambientLight intensity={0.5} />
           <pointLight position={[35, 35, 0]} intensity={0.4} />
           <pointLight position={[-35, 35, 0]} intensity={0.4} />
