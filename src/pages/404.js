7687af2af8962948fc6d2af2d4390f6fbb03d503
juliswer <julianswer36@gmail.com>
2022-07-16 22:16:47 -0300

    suspense

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,17 +6,22 @@ import { OrbitControls } from "@react-three/drei";
 function NotFound() {
   return (
     <>
-      <h2>h</h2>
-      <div style={{height: "100%", width: "100%"}}>
-        <Canvas camera={{ fov: 35, zoom: 20, position: [25, 20, 15] }}>
-          <ambientLight intensity={0.5} />
-          <pointLight position={[35, 35, 0]} intensity={0.4} />
-          <pointLight position={[-35, 35, 0]} intensity={0.4} />
-          <Suspense fallback={null}>
-            <Car />
-          </Suspense>
-          <OrbitControls />
-        </Canvas>
+      <div style={{marginTop: "40px"}}>
+        <div style={{ height: "100%", width: "100%" }}>
+          <Canvas camera={{ fov: 35, zoom: 20, position: [25, 20, 15] }}>
+            <ambientLight intensity={0.5} />
+            <pointLight position={[35, 35, 0]} intensity={0.4} />
+            <pointLight position={[-35, 35, 0]} intensity={0.4} />
+            <Suspense fallback={null}>
+              <Car />
+            </Suspense>
+            <OrbitControls />
+          </Canvas>
+        </div>
+        <h2 style={{color: "#fff"}}>
+          I think you have not found what you where finding, but don&apos;t
+          worry, i give a really nice car to drive wherever you want
+        </h2>
       </div>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
+import { Skeleton } from '@mui/material';
+import React, {Suspense} from 'react';
 import App from "../layout/App";
 
 function Home() {
   return (
     <>
-      <App />
+      <Suspense fallback={<Skeleton variant="rectangular" width={500} heigth={200} />}>
+        <App />
+      </Suspense>
     </>
   );
 }
