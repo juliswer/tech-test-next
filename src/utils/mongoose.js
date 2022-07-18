21de3946d6080cd480142db28a5a84f83e534d85
juliswer <julianswer36@gmail.com>
2022-07-18 11:11:02 -0300

    deploy implementation

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -9,8 +9,9 @@ const conn = {
 // * Function to get the connection
 export async function dbConnect() {
   if (conn.isConnected) return;
-  const db = await connect(process.env.MONGO_URL);
+  const db = await connect(process.env.MONGO_URL ? process.env.MONGO_URL : "mongodb://localhost:27017/techTest");
   conn.isConnected = db.connections[0].readyState;
+  console.log(db.connection.db.databaseName);
 }
 
 // ! Handle "connected" event
