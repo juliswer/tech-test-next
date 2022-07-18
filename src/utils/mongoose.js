50371950e0fc04999b727ad31a5edeaad8150ac6
juliswer <julianswer36@gmail.com>
2022-07-18 00:47:59 -0300

    format

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -1,23 +1,24 @@
+// ? Import Mongoose Libraries
 import { connect, connection } from "mongoose";
 
+// * Initialize Mongoose Connected Variable
 const conn = {
   isConnected: false,
 };
 
+// * Function to get the connection
 export async function dbConnect() {
   if (conn.isConnected) return;
-
   const db = await connect(process.env.MONGO_URL);
-
   conn.isConnected = db.connections[0].readyState;
-
-  console.log(db.connection.db.databaseName);
 }
 
+// ! Handle "connected" event
 connection.on("connected", () => {
   console.log("Mongodb is connected");
 });
 
+// ! Handle "error" event
 connection.on("error", (err) => {
   console.log(`Error Database: ${err}`);
 });
