00c3e9942d6dead6db4bbebb0c062e6c7b96f89f
juliswer <julianswer36@gmail.com>
2022-07-18 01:07:56 -0300

    upload

--- src/helpers/toast_config.js
+++ src/helpers/toast_config.js
@@ -1,8 +1,10 @@
+// * Config for task Event Toasts
 export const taskEventConfig = {
   position: "bottom-center",
   duration: 5000,
 };
 
+// * Config for taskForm Event Toasts
 export const taskFormEventConfig = {
   position: "top-center",
   duration: 5000,
